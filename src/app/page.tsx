6dc047b49e4eaff09b0897a6c4d98a4f2595f829
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-01-29 16:57:28 +0100

    refactorized: added h1, h2, background and new grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default async function Home() {
   return (
     <main className='container mx-auto px-4 py-8 min-h-screen'>
       <header className='mb-8 text-center'>
-        <h1 className='text-3xl sm:text-4xl font-bold text-neutral-1000 mb-2'>
+        <h1 className='text-3xl sm:text-4xl font-bold text-gray-800 mb-2'>
           Dulces Pétalos
         </h1>
-        <h2 className='text-neutral-800'>
+        <h2 className='text-gray-600'>
           Descubre toda la selección de nuestras flores
         </h2>
       </header>
