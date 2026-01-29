c911ef29d35753e28fc85759d1db75efaf7f237f
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-01-30 00:12:26 +0100

    added: typed values for fertilizer

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,6 @@ export interface Product {
     price: number
     imgUrl: string
     wateringsPerWeek: number
-    fertilizerType: string
+    fertilizerType: 'nitrogen' |'phosphorus'
     heightInCm: number
 }
