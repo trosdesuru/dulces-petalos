fbdcf5e9b54a84f629c875a3841f488a8cc783bd
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-01-29 15:17:46 +0100

    added: applied color variables on text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default async function Home() {
 
   return (
     <main className='container mx-auto px-4 py-8 min-h-screen'>
-      <header className='mb-8 text-center sm:text-left'>
-        <h1 className='text-3xl sm:text-4xl font-bold text-gray-800 mb-2'>
-          Dulces Pétalos 🌺
+      <header className='mb-8 text-center'>
+        <h1 className='text-3xl sm:text-4xl font-bold text-neutral-1000 mb-2'>
+          Dulces Pétalos
         </h1>
-        <p className='text-gray-600'>
-          Descubre nuestra selección de flores frescas
-        </p>
+        <h2 className='text-neutral-800'>
+          Descubre toda la selección de nuestras flores
+        </h2>
       </header>
 
       <ProductGrid products={products} />
