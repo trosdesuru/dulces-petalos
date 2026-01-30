923f40fefe4403dd346a0595b4faa1e5df8d2552
Eduard Hernández Ventós <eduardhernandezventos@gmail.com>
2026-01-30 01:18:00 +0100

    refactorized: variables applied on loading and not-found page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
         <main className='container mx-auto px-4 py-8 min-h-screen'>
             {/* Skeleton del Header */}
             <div className='mb-8 space-y-4 animate-pulse'>
-                <div className='h-10 bg-gray-200 rounded w-64 mx-auto sm:mx-0'></div>
+                <div className='h-10 bg-gray-100 rounded w-64 mx-auto sm:mx-0'></div>
                 <div className='h-6 bg-gray-100 rounded w-96 mx-auto sm:mx-0'></div>
             </div>
 
@@ -19,11 +19,11 @@ export default function Loading() {
                         <div className='aspect-4/3 bg-gray-200 w-full'></div>
                         <div className='p-4 space-y-3'>
                             <div className='h-6 bg-gray-200 rounded w-3/4'></div>
-                            <div className='h-4 bg-gray-100 rounded w-full'></div>
-                            <div className='h-4 bg-gray-100 rounded w-2/3'></div>
+                            <div className='h-4 bg-gray-200 rounded w-full'></div>
+                            <div className='h-4 bg-gray-200 rounded w-2/3'></div>
                             <div className='flex justify-between items-center pt-2'>
                                 <div className='h-8 bg-gray-200 rounded w-20'></div>
-                                <div className='h-6 bg-blue-100 rounded w-24'></div>
+                                <div className='h-6 bg-gray-200 rounded w-24'></div>
                             </div>
                         </div>
                     </div>
